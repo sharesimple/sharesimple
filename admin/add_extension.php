feebb8c6068d8634f68e88c8fdc9057c1d231791
CuzImBisonratte <cuzimbisonratte@gmail.com>
2022-08-03 09:54:12 +0200

    moved file extensions to different file

--- admin/add_extension.php
+++ admin/add_extension.php
@@ -4,10 +4,10 @@
     $file_extension = ".".$_POST['extension'];
 
     // Get and decode the json file ../config/settings.json
-    $settings = json_decode(file_get_contents('../config/settings.json'), true);
+    $ext_list = json_decode(file_get_contents('../config/file_extensions.json'), true);
 
     // Push to array
-    array_push($settings["allowed_file_types"], $file_extension);
+    array_push($ext_list, $file_extension);
 
     // Encode json
     $encoded_json = json_encode($settings);
