8ba59d0ae9f76b51ed3a31e80f52e321a9079914
CuzImBisonratte <cuzimbisonratte@gmail.com>
2022-08-09 16:02:11 +0200

    added installation checker

---
+++ setup/scripts/check_installation.php
@@ -0,0 +1,33 @@
+<?php 
+    // Get db connection
+    require("../../config/database.php");
+    
+    // Check if db sharesimple exists
+    $con = mysqli_connect($db_host,$db_user,$db_pass, "sharesimple");
+    if (!isset($con)) {
+        exit("ERROR (101)");
+    } 
+
+    // Check if table "accounts" exists
+    if (!$con->query("DESCRIBE `accounts`")) {
+        exit("ERROR (111)");
+    }
+
+    // Check if table "files" exists
+    if (!$con->query("DESCRIBE `files`")) {
+        exit("ERROR (112)");
+    }
+
+    // Check if settings file exists
+    if(!file_exists("../../config/settings.json")){
+        exit("ERROR (201)");
+    }
+
+    // Check if file_extensions file exists
+    if(!file_exists("../../config/settings.json")){
+        exit("ERROR (202)");
+    }
+
+    // Redirect
+    header("Location: ./step6.html");
+?>
