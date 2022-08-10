0b66e54875b063044f79723e4225c1860ac8a725
CuzImBisonratte <cuzimbisonratte@gmail.com>
2022-08-10 17:26:29 +0200

    deleted old setup

--- setup_old/rewriteIndex.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    // Just rewrite the ../index.php file to redirect to ./app/
-    $file = fopen("../index.php", "w");
-    $txt = "<?php\n\n";
-    $txt .= "header('Location: ./app/');\n";
-    $txt .= "?>";
-    fwrite($file, $txt);
-    fclose($file);
-?>
