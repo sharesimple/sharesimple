ddfa3131003f556f78912f32841863f1e7701e6f
CuzImBisonratte <cuzimbisonratte@gmail.com>
2022-08-10 16:29:43 +0200

    fixed deletor file fail

--- setup/scripts/finish_installation.php
+++ setup/scripts/finish_installation.php
@@ -8,12 +8,12 @@
 
     // Create deleter file
     $deleter_file = "<?php\n";
-    $deleter_file .= "  // Remove setup directory";
-    $deleter_file .= "  rmdir('./setup/');";
-    $deleter_file .= "  // Delete this file";
-    $deleter_file .= "  unlink(__FILE__);";
-    $deleter_file .= "  // Redirect to admin panel";
-    $deleter_file .= "  header('Location: ../../admin/')";
+    $deleter_file .= "  // Remove setup directory\n";
+    $deleter_file .= "  rmdir('./setup/');\n";
+    $deleter_file .= "  // Delete this file\n";
+    $deleter_file .= "  unlink(__FILE__);\n";
+    $deleter_file .= "  // Redirect to admin panel\n";
+    $deleter_file .= "  header('Location: ../../admin/');\n";
     $deleter_file .= "?>";
     file_put_contents("../../del_setup.php", $deleter_file);
 
