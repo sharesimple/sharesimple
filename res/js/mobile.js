fe57d6cc8174f849593862e395ec11fde24bc836
Konstantin Protzen <code@kondev.de>
2024-03-04 20:36:46 +0100

    📱 Add reload on resize

--- res/js/mobile.js
+++ res/js/mobile.js
@@ -6,3 +6,14 @@ if (window.innerWidth <= 700) {
 
 if ((window.matchMedia('(display-mode: standalone)').matches) || (window.navigator.standalone) || document.referrer.includes('android-app://'))
     document.querySelector(".pwa_warning-overlay").style.display = "fixed";
+
+var lastsize = window.innerWidth;
+window.addEventListener('resize', () => {
+    if (window.innerWidth <= 700) {
+        document.querySelector(".main_container_right").replaceWith(...document.querySelector(".main_container_right").childNodes);
+        document.querySelector(".mobile_warning-overlay").style.display = "fixed";
+    } else if (window.innerWidth > lastsize) {
+        window.location.reload();
+    }
+    lastsize = window.innerWidth;
+});
